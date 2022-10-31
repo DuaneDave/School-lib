245efa3ea81e8def2e3e08bb2006759e6313fdd4
Duane David <duanedave17@gmail.com>
2022-10-31 04:19:52 -0700

    Initialize a person class

---
+++ person.rb
@@ -0,0 +1,18 @@
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, parent_permission: true, name: 'Unknown')
+    @id = id
+    @name = name
+    @age = age
+  end
+
+  def is_of_age?
+    age >= 18
+  end
+
+  def can_use_services?
+    is_of_age? || parent_permission
+  end
+end
