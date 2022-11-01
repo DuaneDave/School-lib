b3a289cbad9c95d169ea1215ca3c5f6dd76977c6
Duane David <duanedave17@gmail.com>
2022-11-01 04:08:21 -0700

    Resolve linter error

--- trimmer_decoraator.rb
+++ trimmer_decoraator.rb
@@ -2,6 +2,6 @@ require_relative 'decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    @nameable.name.strip.slice(0, 10)
+    @nameable.name.strip.slice(0, 10) if @nameable.name.length > 10
   end
 end
