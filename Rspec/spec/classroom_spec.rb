18ac2dbae97026d7d5fbfb6d0a500e637c0fd67b
Duane David <duanedave17@gmail.com>
2022-11-10 03:52:03 -0800

    Resolve linter error

--- Rspec/spec/classroom_spec.rb
+++ Rspec/spec/classroom_spec.rb
@@ -1,8 +1,8 @@
 require_relative './../../classroom'
 
 describe Classroom do
-  context "When testing the Classroom class" do
-    it "should create a new classroom when Classroom class is instantiated" do
+  context 'When testing the Classroom class' do
+    it 'should create a new classroom when Classroom class is instantiated' do
       cl = Classroom.new('1E')
       expect(cl.label).to eq '1E'
     end
