a6eea21a9d6e506fea8100ade3877575a381183a
Duane David <duanedave17@gmail.com>
2022-11-10 03:46:13 -0800

    Resolve linter error

--- Rspec/spec/student_spec.rb
+++ Rspec/spec/student_spec.rb
@@ -1,8 +1,8 @@
 require_relative './../../student'
 
 describe Student do
-  context "When testing the Student class" do
-    it "should create a new Student when student class is instantiated" do
+  context 'When testing the Student class' do
+    it 'should create a new Student when student class is instantiated' do
       student = Student.new(23, 'William Raga', true)
       expect(student.age).to eq 23
       expect(student.name).to eq 'William Raga'
